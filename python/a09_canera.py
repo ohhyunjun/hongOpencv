f2f3e6e2b48076ca51a4b914e1e62ce47974ea03
ohhyunjun <dhguswns75@gmail.com>
2025-09-05 15:28:30 +0900

    feat: 웹캠 코드 정상작동 코드

--- python/a09_canera.py
+++ python/a09_canera.py
@@ -9,7 +9,12 @@ def put_string(frame, text, pt, value, color=(120, 200, 90)):             # 문�
     cv2.putText(frame, text, pt, font, 0.7, (120, 200, 90), 2)  # 글자 적기
 
 def main():
-    cap = cv2.VideoCapture("/home/hyunjun/hongOpencv/data/vtest.avi")
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
 
     print(f"너비 {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}")
     print(f"높이 {cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}")
